7bf73a7066d5661641d5be05620af33febd1256e
cfdonmez <90632887+cfdonmez@users.noreply.github.com>
2021-11-06 23:50:04 +0300

    Update HurricaneExercise.m

--- HurricaneExercise.m
+++ HurricaneExercise.m
@@ -10,3 +10,20 @@
   xlabel("Wind Speed (mph)")
   ylabel("Pressure (mbar)")
   hold off
+
+
+
+%Hurricane Exercises Part 2
+
+data = readtable("hurricanes.txt");
+data.Location = categorical(data.Location);
+onland = data.Location=="Land";
+scatter(data.Windspeed(~onland),data.Pressure(~onland),"b","filled","MarkerFaceAlpha",0.5)
+hold on
+scatter(data.Windspeed(onland),data.Pressure(onland),"r","filled","MarkerFaceAlpha",0.5)
+hold off
+xlabel("Wind Speed (mph)")
+ylabel("Pressure (mbar)")
+
+xlim([15 60])
+grid('minor')
