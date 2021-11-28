11297e23c6da0e139dfc1aea50a7c92d8453bf43
cfdonmez <cfdonmez@gmail.com>
2021-11-28 10:48:46 +0300

    sadeleştirme
    gaAlgo(3,[-10 0 0],[10 5 5]) yazman yeterli

--- Optimization101/haticeOdev/gaAlgo.m
+++ Optimization101/haticeOdev/gaAlgo.m
@@ -1,7 +1,7 @@
 % bu kýsým genetik algoritma yöntemiyle minimum deðeri buluyor
 % population size = 20
 % iterasyon = 50 olarak ayarlandý
-function [x,fval,exitflag,output,population,score] = gaAlgo(nvars,lb,ub,PopulationSize_Data)
+function [x,fval,exitflag,output,population,score] = gaAlgo(nvars,lb,ub)
 
 
 options = optimoptions('ga');
@@ -9,8 +9,9 @@ options = optimoptions('ga');
 
 options = optimoptions(options,'PopulationSize', 20);
 options = optimoptions(options,'MaxStallGenerations',25,'MaxGenerations',50)
-options = optimoptions(options,'Display', 'iter');
+%options = optimoptions(options,'Display', 'iter');
 options = optimoptions(options,'PlotFcn', { @gaplotbestf });
+
 [x,fval,exitflag,output,population,score] = ...
 ga(@hatice,nvars,[],[],[],[],lb,ub,[],[],options);
 
