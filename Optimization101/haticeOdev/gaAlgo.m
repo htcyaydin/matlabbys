66c838266d5bcfe5c25c64cb470ecf612933ee2a
cfdonmez <cfdonmez@gmail.com>
2021-11-28 10:31:47 +0300

    Update Algo
    Açıklamalar eklendi

--- Optimization101/haticeOdev/gaAlgo.m
+++ Optimization101/haticeOdev/gaAlgo.m
@@ -1,10 +1,13 @@
+% bu kýsým genetik algoritma yöntemiyle minimum deðeri buluyor
+% population size = 20
+% iterasyon = 50 olarak ayarlandý
 function [x,fval,exitflag,output,population,score] = gaAlgo(nvars,lb,ub,PopulationSize_Data)
 
 
 options = optimoptions('ga');
 
 
-options = optimoptions(options,'PopulationSize', PopulationSize_Data);
+options = optimoptions(options,'PopulationSize', 20);
 options = optimoptions(options,'MaxStallGenerations',25,'MaxGenerations',50)
 options = optimoptions(options,'Display', 'iter');
 options = optimoptions(options,'PlotFcn', { @gaplotbestf });
